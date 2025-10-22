8448ed00d7c96e053b77867ae31c0f20ad0ee0ea
Hilda-Baraiywo <baraiywohilda181@gmail.com>
2025-10-22 10:42:54 +0300

    Refactor MongoDB connection to use connectDB
    Replaced direct MongoDB connection with connectDB function.

--- server.js
+++ server.js
@@ -16,11 +16,7 @@ const PORT = process.env.PORT || 3000;
 const validApiKey = process.env.API_KEY;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+connectDB();
 
 // Middleware setup
 app.use(express.json());
